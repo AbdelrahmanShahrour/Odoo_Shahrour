6d88445f4571d321085d4bf96e9e9d4c2aa1ecd9
AbdelrahmanShahrour <abdalrahman_shahrour@outlook.com>
2023-12-31 10:14:44 +0300

    add new endpoints for stock

--- controller_web/controller/stock_controller.py
+++ controller_web/controller/stock_controller.py
@@ -9,6 +9,15 @@ class StockController(http.Controller):
     - POST /api/stock (create)  --not implemented
     - PUT /api/stock    (update) --not implemented
     - DELETE /api/stock (delete) --not implemented
+
+    - GET /api/stock/<id> (read) --not implemented
+    - POST /api/stock/<id> (create) --not implemented
+    - PUT /api/stock/<id> (update) --not implemented
+    - DELETE /api/stock/<id> (delete) --not implemented
+
+    this endpoint '/api/stock' is responsible for returning the stock data
+    - GET /api/stock
+    -- return the stock data
     '''
     @http.route('/api/stock', auth='user', type='http', methods=['GET'])
     def get_stock_data(self, **kwargs):
